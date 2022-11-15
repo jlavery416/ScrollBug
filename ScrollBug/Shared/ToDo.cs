22b53bff5f977c8aa967b5316d7c7e012eefc8aa
JLavery <JLavery@MaxetaTech.com>
2022-11-15 15:53:59 -0600

    initial

---
+++ ScrollBug/Shared/ToDo.cs
@@ -0,0 +1,8 @@
+﻿namespace ScrollBug.Shared {
+  public class ToDo {
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; } = String.Empty;
+    public bool Completed { get; set; }
+  }
+}
